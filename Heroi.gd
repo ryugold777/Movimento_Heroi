e79bca78abb6099519a24c6f3341351d2075b67a
Ryu777 <76182721+ryugold777@users.noreply.github.com>
2022-03-01 04:23:00 -0300

    Create Heroi.gd

---
+++ Heroi.gd
@@ -0,0 +1,52 @@
+extends KinematicBody2D
+
+# Declaração das vairáveis
+var rapidez = 80
+var velocidade = Vector2()
+
+onready var Heroi = $AnimatedSprite # O nome do nó do herói
+
+func pegar_teclas():
+	# Detecta se as teclas das setas do teclado foram pressionadas
+	velocidade = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		Heroi.play("Lado_Direito")
+		velocidade.y += 1
+		velocidade.x += 1
+	if Input.is_action_pressed('ui_left'):
+		Heroi.play("Lado_Esquerdo")
+		velocidade.x -= 1
+		velocidade.y -= 1
+	if Input.is_action_pressed('ui_left') && Input.is_action_pressed("ui_down"):
+		Heroi.play("")
+		velocidade.x = 0
+		velocidade.y = 0
+	if Input.is_action_pressed('ui_down'):
+		Heroi.play("De_Frente")
+		velocidade.y += 1
+		velocidade.x -= 1
+	if Input.is_action_pressed('ui_right') && Input.is_action_pressed("ui_down"):
+		Heroi.play("")
+		velocidade.x = 0
+		velocidade.y = 0
+	if Input.is_action_pressed('ui_up'):
+		Heroi.play("De_Costa")
+		velocidade.y -= 1
+		velocidade.x += 1
+	if Input.is_action_pressed('ui_right') && Input.is_action_pressed("ui_up"):
+		Heroi.play("")
+		velocidade.x = 0
+		velocidade.y = 0
+	if Input.is_action_pressed('ui_left') && Input.is_action_pressed("ui_up"):
+		Heroi.play("")
+		velocidade.x = 0
+		velocidade.y = 0
+	if velocidade.length() > 0:
+		velocidade = velocidade.normalized() * rapidez
+		Heroi.play()
+	else:
+		Heroi.stop()
+
+func _physics_process(delta):
+	pegar_teclas()
+	move_and_collide(velocidade * delta)
